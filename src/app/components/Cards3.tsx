c543b21d268e7adc16a23518cc6b79fb5044e5dd
unknown <umergulkaleem+github@gmail.com>
2024-11-01 15:38:55 +0500

    initial commit

---
+++ src/app/components/Cards3.tsx
@@ -0,0 +1,31 @@
+export default function Cards3() {
+  return (
+    <div>
+      <div className="flex justify-center gap-10 mx-8 mt-5">
+        <div className="bg-white text-black p-20 ">
+          <h1 className="font-bold text-lg">Card title</h1>
+          <p>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content
+          </p>
+        </div>
+        <div className="bg-blue-400 text-black p-20 ">
+          {" "}
+          <h1 className="font-bold text-lg">Card title</h1>
+          <p>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content
+          </p>
+        </div>
+        <div className="bg-white text-black p-20 ">
+          {" "}
+          <h1 className="font-bold text-lg">Card title</h1>
+          <p>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Cards3 from "./components/Cards3";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="flex text-lg justify-center items-center mt-2 ">
+        Hello World
+      </h1>
+      <Cards3 />
+    </div>
+  );
+}
